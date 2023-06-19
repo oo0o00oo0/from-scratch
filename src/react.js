88451c2597cc5198ad257eaaa51177176c39d6c2
oo0o00oo0 <dsgnrm@gmail.com>
2023-06-20 00:45:36 +0100

    test

--- src/react.js
+++ src/react.js
@@ -334,7 +334,7 @@ const SomeRenderer = {
 
 function Counter() {
   const [state, setState] = SomeRenderer.useState(1)
-  return <h1 onClick={() => setState((c) => c + 1)}>Count: {state}</h1>
+  return <h1 onClick={() => setState((c) => c + 1)}>Counting state: {state}</h1>
 }
 const element = <Counter />
 const container = document.getElementById("root")
